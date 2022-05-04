8abb42860e641a211019c8c73eaf7b1a12d3d62e
Michal Skop <michal.skop@kohovolit.eu>
2022-05-04 23:53:46 +0200

    meta 4

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,33 +12,35 @@ export default defineNuxtConfig({
   //   transpile: ['echarts']
   // }
   // https://github.com/nuxt/framework/pull/2655
-  head: {
-    title: 'Mandáty.cz',
-    meta: [
-      // { charset: 'utf-8' },
-      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Nejpřesnější denně aktualizované volební modely.'
-      },
-      {
-        hid: 'og:type',
-        name: 'og:type',
-        content: 'website'
-      },
-      {
-        hid: 'fb:app_id',
-        name: 'fb:app_id',
-        content: '493242628099686'
-      },
-      {
-        hid: 'twitter:creator',
-        name: 'twitter:creator',
-        content: '@skopmichal'
-      },
-      
-    ],
-    link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+  app: {
+    head: {
+      title: 'Mandáty.cz',
+      meta: [
+        // { charset: 'utf-8' },
+        // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nejpřesnější denně aktualizované volební modely.'
+        },
+        {
+          hid: 'og:type',
+          name: 'og:type',
+          content: 'website'
+        },
+        {
+          hid: 'fb:app_id',
+          name: 'fb:app_id',
+          content: '493242628099686'
+        },
+        {
+          hid: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@skopmichal'
+        },
+        
+      ],
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+    }
   }
 })
