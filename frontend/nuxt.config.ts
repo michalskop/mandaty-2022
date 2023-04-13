ed034f17578f8f3561fdffff233b419ad2a9988b
Michal Skop <michal.skop@kohovolit.eu>
2023-04-13 23:42:31 +0200

    matomo, close #4

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true
+  ssr: true,
+  plugins: [
+    { src: '~/plugins/matomo-plugin.client.js', ssr:false }
+  ]
 })
