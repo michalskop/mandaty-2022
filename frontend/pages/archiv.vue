3ef59bf8ad02abb01f4b0602413543e231800626
Michal Skop <michal.skop@kohovolit.eu>
2023-11-12 21:45:51 +0100

    update kantar

--- frontend/pages/archiv.vue
+++ frontend/pages/archiv.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <h1 class="pt-4">Archiv projektů</h1>
-    <ul>
-      <li><NuxtLink to="/president-2023">Presidentské volby 2023</NuxtLink></li>
-      <li><a href="https://2021.mandaty.cz/">Parlamentní volby 2021</a></li>
+    <ul class="alert alert-info list-group">
+      <li class="list-group-item"><NuxtLink to="/president-2023">Presidentské volby 2023</NuxtLink></li>
+      <li class="list-group-item"><a href="https://2021.mandaty.cz/">Parlamentní volby 2021</a></li>
     </ul>
   </div>
 </template>
